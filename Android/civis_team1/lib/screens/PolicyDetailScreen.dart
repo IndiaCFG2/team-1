089bb748838e4b4398afb9835926478699457bf3
Mahima M A <43963605+Mahima-M-A@users.noreply.github.com>
2020-08-08 20:07:33 +0530

    Update PolicyDetailScreen.dart

--- Android/civis_team1/lib/screens/PolicyDetailScreen.dart
+++ Android/civis_team1/lib/screens/PolicyDetailScreen.dart
@@ -1,14 +1,59 @@
 import 'package:flutter/material.dart';
 
-class PolicyDetailScreen extends StatelessWidget {
+class PolicyDetailScreen extends StatefulWidget {
   static const routeName='policydetails';
+  @override
+  _PolicyDetailScreenState createState() => _PolicyDetailScreenState();
+}
+
+class _PolicyDetailScreenState extends State<PolicyDetailScreen> {
+  bool descTextShowFlag = true;
   @override
   Widget build(BuildContext context) {
     return 
     Scaffold(
+      appBar: AppBar(
+        title: Text('Details'),
+      ),
       body: Container(
-      child: Text('Details'),
-    ) ,
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Details of the policy will be displayed here',
+              maxLines: descTextShowFlag ? 50 : 4,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                  fontSize: 14,
+              ),
+            ),
+            InkWell(
+              onTap: (){ //to show full or minimal description of book
+                setState(() {
+                  descTextShowFlag = !descTextShowFlag;
+                });
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  descTextShowFlag ? Text("Show Less",style: TextStyle(color: Colors.blue),) :  Text("Show More",style: TextStyle(color: Colors.blue))
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            RaisedButton(
+              color: Theme.of(context).primaryColor,
+              padding: EdgeInsets.symmetric(vertical: 5,horizontal: 40),
+              child: Text(
+                'Response',
+                style: TextStyle(color: Colors.white),
+              ),
+              //onPressed: function(),
+              ),
+          ]
+        )
+      ) ,
     );
     
    
