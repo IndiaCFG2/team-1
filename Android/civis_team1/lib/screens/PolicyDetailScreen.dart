ffa92241a35ba3818eb8676b952c4ca5a6e0e84a
Mahima M A <43963605+Mahima-M-A@users.noreply.github.com>
2020-08-08 21:19:54 +0530

    Update PolicyDetailScreen.dart

--- Android/civis_team1/lib/screens/PolicyDetailScreen.dart
+++ Android/civis_team1/lib/screens/PolicyDetailScreen.dart
@@ -15,47 +15,58 @@ class _PolicyDetailScreenState extends State<PolicyDetailScreen> {
       appBar: AppBar(
         title: Text('Details'),
       ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Text(
-              'Details of the policy will be displayed here',
-              maxLines: descTextShowFlag ? 50 : 4,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(
-                  fontSize: 14,
-              ),
-            ),
-            InkWell(
-              onTap: (){ //to show full or minimal description of book
-                setState(() {
-                  descTextShowFlag = !descTextShowFlag;
-                });
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
+      body: Card(
+          child: Material(
+              color: Colors.white70,
+              child: Column(
                 children: <Widget>[
-                  descTextShowFlag ? Text("Show Less",style: TextStyle(color: Colors.blue),) :  Text("Show More",style: TextStyle(color: Colors.blue))
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            RaisedButton(
-              color: Theme.of(context).primaryColor,
-              padding: EdgeInsets.symmetric(vertical: 5,horizontal: 40),
-              child: Text(
-                'Response',
-                style: TextStyle(color: Colors.white),
-              ),
-              //onPressed: function(),
-              ),
-          ]
-        )
-      ) ,
+                  Text(
+                    'Policy Title',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    'Details of the policy will be displayed here',
+                    maxLines: descTextShowFlag ? 50 : 4,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black87,
+                    ),
+                  ),
+                  InkWell(
+                    onTap: (){ //to show full or minimal description of book
+                      setState(() {
+                        descTextShowFlag = !descTextShowFlag;
+                      });
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        descTextShowFlag ? Text("Show Less",style: TextStyle(color: Colors.blue),) :  Text("Show More",style: TextStyle(color: Colors.blue))
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  RaisedButton(
+                    color: Theme.of(context).primaryColor,
+                    padding: EdgeInsets.symmetric(vertical: 5,horizontal: 40),
+                    child: Text(
+                      'Response',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    //onPressed: function(),
+                    ),
+                ]
+              )
+          )
+      )
     );
-    
-   
   }
 }
